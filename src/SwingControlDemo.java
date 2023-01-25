ce2d5e88348adbff4b57c676702de62cc78ee620
laurianie guiteau <laurianie_guiteau24@milton.edu>
2023-01-24 21:49:31 -0500

    Initial commit

---
+++ src/SwingControlDemo.java
@@ -0,0 +1,125 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class SwingControlDemo implements ActionListener {
+    private JFrame mainFrame;
+    private JLabel headerLabel;
+    private JLabel statusLabel;
+    private JPanel controlPanel;
+    private JMenuBar mb;
+    private JMenu file, edit, help;
+    private JMenuItem explode, c0py, p4ste, selectAll;
+    private JTextArea ta;
+    private int WIDTH=400;
+    private int HEIGHT=400;
+
+
+    public SwingControlDemo() {
+        prepareGUI();
+    }
+
+    public static void main(String[] args) {
+        SwingControlDemo swingControlDemo = new SwingControlDemo();
+        swingControlDemo.showEventDemo();
+    }
+
+    private void prepareGUI() {
+        mainFrame = new JFrame("Java SWING Examples");
+        mainFrame.setSize(WIDTH, HEIGHT);
+        mainFrame.setLayout(new GridLayout(3, 1));
+
+
+        explode = new JMenuItem("explode");
+        c0py = new JMenuItem("c0py");
+        p4ste = new JMenuItem("p4ste");
+        selectAll = new JMenuItem("selectAll");
+        explode.addActionListener(this);
+        c0py.addActionListener(this);
+        p4ste.addActionListener(this);
+        selectAll.addActionListener(this);
+
+        mb = new JMenuBar();
+        file = new JMenu("File");
+        edit = new JMenu("Edit");
+        help = new JMenu("Help");
+        edit.add(explode);
+        edit.add(c0py);
+        edit.add(p4ste);
+        edit.add(selectAll);
+        mb.add(file);
+        mb.add(edit);
+        mb.add(help);
+
+        ta = new JTextArea();
+        ta.setBounds(50, 5, WIDTH-100, HEIGHT-50);
+        mainFrame.add(mb);
+        mainFrame.add(ta);
+        mainFrame.setJMenuBar(mb);
+
+        headerLabel = new JLabel("", JLabel.CENTER);
+        statusLabel = new JLabel("", JLabel.CENTER);
+        statusLabel.setSize(350, 100);
+
+        mainFrame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent) {
+                System.exit(0);
+            }
+        });
+        controlPanel = new JPanel();
+        controlPanel.setLayout(new FlowLayout());
+
+       // mainFrame.add(headerLabel);
+        mainFrame.add(controlPanel);
+        mainFrame.add(statusLabel);
+        mainFrame.setVisible(true);
+    }
+
+    private void showEventDemo() {
+        headerLabel.setText("Control in action: Button");
+
+        JButton okButton = new JButton("OK");
+        JButton submitButton = new JButton("Submit");
+        JButton cancelButton = new JButton("Cancel");
+
+        okButton.setActionCommand("OK");
+        submitButton.setActionCommand("Submit");
+        cancelButton.setActionCommand("Cancel");
+
+        okButton.addActionListener(new ButtonClickListener());
+        submitButton.addActionListener(new ButtonClickListener());
+        cancelButton.addActionListener(new ButtonClickListener());
+
+        controlPanel.add(okButton);
+        controlPanel.add(submitButton);
+        controlPanel.add(cancelButton);
+
+        mainFrame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == explode)
+            ta.cut();
+        if (e.getSource() == p4ste)
+            ta.paste();
+        if (e.getSource() == c0py)
+            ta.copy();
+        if (e.getSource() == selectAll)
+            ta.selectAll();
+    }
+
+    private class ButtonClickListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String command = e.getActionCommand();
+
+            if (command.equals("OK")) {
+                statusLabel.setText("Ok Button clicked.");
+            } else if (command.equals("Submit")) {
+                statusLabel.setText("Submit Button clicked.");
+            } else {
+                statusLabel.setText("Cancel Button clicked.");
+            }
+        }
+    }
+}
